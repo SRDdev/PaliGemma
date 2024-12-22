306bb164c3b266fbf55128ba7ace230ef4c4888b
SRDdev <shreyasrd31@gmail.com>
2024-12-22 16:02:26 +0530

    Load hf models

--- modules/utils.py
+++ modules/utils.py
@@ -6,6 +6,13 @@ import numpy as np
 import torch
 from PIL import Image
 from torchvision import transforms
+from transformers import AutoTokenizer
+from glob import glob
+from safetensors import safe_open
+import os
+import json
+from typing import Tuple
+from multimodal import PaliGemmaForConditionalGeneration, MultiModalConfig
 
 def load_image(image_path: str) -> torch.Tensor:
     """
@@ -35,3 +42,34 @@ def load_config(config_path="config.yaml"):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
+
+def load_hf_model(model_path: str, device: str) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+    # Load the tokenizer
+    tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="right")
+    assert tokenizer.padding_side == "right"
+
+    # Find all the *.safetensors files
+    safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
+
+    # ... and load them one by one in the tensors dictionary
+    tensors = {}
+    for safetensors_file in safetensors_files:
+        with safe_open(safetensors_file, framework="pt", device="cpu") as f:
+            for key in f.keys():
+                tensors[key] = f.get_tensor(key)
+
+    # Load the model's config
+    with open(os.path.join(model_path, "config.json"), "r") as f:
+        model_config_file = json.load(f)
+        config = MultiModalConfig(**model_config_file)
+
+    # Create the model using the configuration
+    model = PaliGemmaForConditionalGeneration(config).to(device)
+
+    # Load the state dict of the model
+    model.load_state_dict(tensors, strict=False)
+
+    # Tie weights
+    model.tie_weights()
+
+    return (model, tokenizer)
